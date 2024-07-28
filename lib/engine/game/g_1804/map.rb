d2e4357ee0119092b2ef48ab1b059e1d72feff26
jclancy15 <josephmclancy@gmail.com>
2024-07-27 20:46:29 -0400

    Update map.rb

--- lib/engine/game/g_1804/map.rb
+++ lib/engine/game/g_1804/map.rb
@@ -113,11 +113,7 @@ module Engine
           gray: {
             %w[B13 I18] => 'town=revenue:10;path=a:0,b:_0;path=a:1,b:_0',
             ['D1'] => 'town=revenue:10;path=a:3,b:_0;path=a:5,b:_0',
-            %w[G2 J17] => 'path=a:2,b:3',
-            %w[F1 I6] => 'path=a:3,b:5',
-            ['B5'] => 'path=a:0,b:4',
-            ['D13'] => 'path=a:0,b:3',
-            ['C2'] => 'path=a:0,b:3; path=a:3,b:5',
+
           },
 
           yellow: {
