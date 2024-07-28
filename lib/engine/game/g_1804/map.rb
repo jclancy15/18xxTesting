73bf09c485861c78e8bbb0a46db14d9427d76721
jclancy15 <josephmclancy@gmail.com>
2024-07-27 20:07:11 -0400

    Update map.rb

--- lib/engine/game/g_1804/map.rb
+++ lib/engine/game/g_1804/map.rb
@@ -107,64 +107,6 @@ module Engine
             ['D13'] => 'path=a:0,b:3',
             ['C2'] => 'path=a:0,b:3; path=a:3,b:5',
           },
-          white: {
-            %w[F4 J14 F22] => 'city=revenue:0;upgrade=cost:80,terrain:water',
-            ['E7'] => 'town=revenue:0;border=edge:5,type:impassable',
-            ['F8'] => 'border=edge:2,type:impassable',
-            ['C11'] => 'border=edge:5,type:impassable',
-            ['C13'] => 'border=edge:0,type:impassable',
-            ['D12'] => 'border=edge:2,type:impassable;border=edge:3,type:impassable',
-            ['B16'] => 'city=revenue:0;border=edge:5,type:impassable',
-            ['C17'] => 'upgrade=cost:120,terrain:mountain;border=edge:2,type:impassable',
-            %w[B20 D4 F10] => 'town',
-            %w[I13
-               D18
-               B12
-               B14
-               B22
-               C7
-               C9
-               C23
-               D8
-               D16
-               D20
-               E3
-               E13
-               E15
-               F12
-               F14
-               F18
-               G3
-               G5
-               G9
-               G11
-               H2
-               H6
-               H8
-               H14
-               I3
-               I5
-               I7
-               I9
-               J4
-               J6
-               J8] => 'blank',
-            %w[G15 C21 D22 E17 E21 G13 I11 J10 J12] =>
-            'upgrade=cost:120,terrain:mountain',
-            %w[E19 H4 B10 H10 H16] => 'city',
-            ['F16'] => 'city=revenue:0;upgrade=cost:120,terrain:mountain',
-            %w[G7 G17 F20] => 'town=revenue:0;town=revenue:0',
-            %w[D6 I17 B18 C19] => 'upgrade=cost:80,terrain:water',
-          },
-          yellow: {
-            %w[E5 D10] =>
-                     'city=revenue:0;city=revenue:0;label=OO;upgrade=cost:80,terrain:water',
-            %w[E11 H18] => 'city=revenue:0;city=revenue:0;label=OO',
-            ['I15'] => 'city=revenue:30;path=a:4,b:_0;path=a:0,b:_0;label=B',
-            ['G19'] =>
-            'city=revenue:40;city=revenue:40;path=a:3,b:_0;path=a:0,b:_1;label=NY;upgrade=cost:80,terrain:water',
-            ['E23'] => 'city=revenue:30;path=a:3,b:_0;path=a:5,b:_0;label=B',
-          },
         }.freeze
 
         LAYOUT = :pointy
